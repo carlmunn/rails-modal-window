410db540a640dcbbab062d820ea1ee111c6029ff
Carl Munn <carl.munn@open2view.com>
2017-10-02 15:49:01 +1300

    Module restructure
    - Order of module load isn't always the name

--- app/helpers/modal_window_helper.rb
+++ app/helpers/modal_window_helper.rb
@@ -1,16 +1,18 @@
-module ModalWindow::ModalWindowHelper
-  def insert_modal_window(klass: nil, selector: nil, &block)
+module ModalWindow
+  module ModalWindowHelper
+    def insert_modal_window(klass: nil, selector: nil, &block)
 
-    data     = nil
-    data     = {selector: selector} if selector
-    _content = capture(&block) if block_given?
-    
-    div_centered = tag.div(class: 'modal-full-window') do
-      tag.div(_content, class: ['modal-item', klass].compact.join(' '), data: data)
-    end
+      data     = nil
+      data     = {selector: selector} if selector
+      _content = capture(&block) if block_given?
+      
+      div_centered = tag.div(class: 'modal-full-window') do
+        tag.div(_content, class: ['modal-item', klass].compact.join(' '), data: data)
+      end
 
-    tag.div(class: 'modal-main animated') do
-      [ div_centered, tag.div(nil, class: 'modal-background action-modal-window')].join.html_safe
+      tag.div(class: 'modal-main animated') do
+        [ div_centered, tag.div(nil, class: 'modal-background action-modal-window')].join.html_safe
+      end
     end
   end
 end
