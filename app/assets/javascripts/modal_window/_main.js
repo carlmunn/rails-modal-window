28f52a93de480f42b0b32721f9fc0d9d0f72a9c0
Carl Munn <carl.munn@open2view.com>
2017-10-04 09:13:19 +1300

    Add template option and CSS selector generator
    - Helps with scoping to individual modals if there is more than one
      method is #_genSel
    - Added #_renderTemplate handles DOM templates and a function as a option
      which can be used to completely replace the generator

--- app/assets/javascripts/modal_window/_main.js
+++ app/assets/javascripts/modal_window/_main.js
@@ -1,15 +1,35 @@
 const ModalWindow = function(opts){
 
-  // opts.content
-  // opts.id
+  // opts.namespace
+  // opts.template
 
   this.opts = opts;
 
   const animateForward = 'modal-visible-animated-in';  
   const obj            = this;
+  var   modalSelector  = '.modal-main'
+  var   promise        = nil //new Promise;
+
+  // Specific modal window
+  if(opts.namespace) modalSelector = modalSelector+'.'+opts.namespace;
 
   const _log = function(msg) {
     if(console) console.log('[L][ModalWindow]', msg);
+  }
+
+  // Selector generator that scopes to the namespace
+  // Passing nothing will just give the parent "modal-main"
+  const _genSel = function(selector){
+    
+    if(!selector) return modalSelector;
+
+    var ary = selector.split(',').map($.proxy(function(str){
+      return [modalSelector, str.trim()].join(' ');
+    }, this));
+
+    var result = ary.join(',');
+
+    return ary.join(',');
   }
 
   const isOpen = function(){
@@ -24,14 +44,51 @@ const ModalWindow = function(opts){
     }, 300);
   }
 
+  // +template+ can be a DOM to a function
+  //
+  // DOM it will call .html() to get the contents to fill
+  //
+  // Function it will call and use the returned contents
+  // if false it doesn't fill so one can do their own.
+  const _renderTemplate = function(){
+
+    if(!opts.template) return;
+
+    var str = "";
+
+    if(typeof(opts.template)=='function') {
+      _log('function template, calling it');
+      str = opts.template();
+    } else {
+      _log("Rendering template: '"+opts.template+"'");
+      str = $(opts.template).html();
+    }
+    
+    if(str!=false) $(_genSel('.modal-item')).html(str);
+  }
+
+  const _openModal = function(){
+    _log("Opening modal: '"+modalSelector+"'");
+    $('.modal-main').removeClass('selected')
+    $(_genSel()).addClass('selected');
+  }
+
   // Stops the background from scrolling
   const safariFix = function(enable){
     $('body').toggleClass('modal-safari-fix', enable)
   }
 
+  this.generateSelector = function(selector){
+    return _genSel(selector);
+  }
+
   this.open = function(){
     
     _log("Opening");
+
+    _renderTemplate();
+
+    _openModal();
 
     $('body').addClass('modal-visible').addClass(animateForward);
 
@@ -45,6 +102,8 @@ const ModalWindow = function(opts){
     $('body').addClass('modal-visible-animated-out');
     delayedClassRemove();
     safariFix(false);
+
+    return promise.resolve()
   }
 
   this.toggle = function(){
@@ -56,7 +115,7 @@ const ModalWindow = function(opts){
 
   this.attachEvents = function(selector){
 
-    if(!selector) selector = '.action-modal-window';
+    if(!selector) selector = _genSel('.action-modal-window');
 
     _log("Attaching events to '"+selector+"'");
 
@@ -68,7 +127,7 @@ const ModalWindow = function(opts){
     })
   }
 
-  _log("Loaded");
+  _log("Loaded: '"+modalSelector+"'");
 
   return this;
 }
--- app/helpers/modal_window_helper.rb
+++ app/helpers/modal_window_helper.rb
@@ -1,18 +1,26 @@
 module ModalWindow
   module ModalWindowHelper
-    def insert_modal_window(klass: nil, selector: nil, &block)
+
+    # +namespace+ Scopes the DIV elenents into something we can style or scope to
+    # +selector+ Forgot...
+    def insert_modal_window(namespace: nil, selector: nil, &block)
 
       data     = nil
       data     = {selector: selector} if selector
       _content = capture(&block) if block_given?
       
-      div_centered = tag.div(class: 'modal-full-window') do
-        tag.div(_content, class: ['modal-item', klass].compact.join(' '), data: data)
+      div_centered = tag.div(class: _klasses('modal-full-window', namespace)) do
+        tag.div(_content, class: 'modal-item', data: data)
       end
 
-      tag.div(class: 'modal-main animated') do
+      tag.div(class: _klasses('modal-main', 'animated', namespace)) do
         [ div_centered, tag.div(nil, class: 'modal-background action-modal-window')].join.html_safe
       end
+    end
+
+  private
+    def _klasses(*klasses)
+      [*klasses].reject {|str| str.blank? }.join(' ')
     end
   end
 end
